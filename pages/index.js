b078f249f7a3fc8cb1660315372bbeee0ed1a2fa
prakharseth <56907863+prakharseth@users.noreply.github.com>
2022-08-08 23:53:57 +0530

    file remove and title change

--- pages/index.js
+++ pages/index.js
@@ -13,32 +13,44 @@ export default function index() {
     setInput("");
   };
   return (
-
     <div>
       <Head>
         <title>Todo App</title>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700&display=swap" rel="stylesheet"></link>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700&display=swap'
+          rel='stylesheet'
+        ></link>
       </Head>
-      <div className="flex mt-32 justify-center h-screen font-nunito">
-        <div className="flex flex-col items-center">
-
-          <div className="text-3xl font-bold text-slate-700">Welcome to My Todo App</div>
+      <div className='flex mt-32 justify-center h-screen font-nunito'>
+        <div className='flex flex-col items-center'>
+          <div className='text-3xl font-bold text-slate-700'>TODO APP</div>
 
           <form>
-            <input value={input} onChange={e => setInput(e.target.value)} className="border-[1.4px] rounded-sm h-8 mt-10 pl-2 w-80 border-slate-600"
-              placeholder="Enter Your Task" type="text" />
+            <input
+              value={input}
+              onChange={e => setInput(e.target.value)}
+              className='border-[1.4px] rounded-sm h-8 mt-10 pl-2 w-80 border-slate-600'
+              placeholder='Enter Your Task'
+              type='text'
+            />
 
-            <button type="submit" onClick={addTodo} className="mt-4 border p-2 rounded-sm text-center bg-slate-700 text-white">Add Todo</button>
+            <button
+              type='submit'
+              onClick={addTodo}
+              className='mt-4 border p-2 rounded-sm text-center bg-slate-700 text-white'
+            >
+              Add Todo
+            </button>
           </form>
 
-          <div className="text-2xl font-semibold text-gray-600 mt-4 underline">List of Tasks</div>
+          <div className='text-2xl font-semibold text-gray-600 mt-4 underline'>
+            List of Tasks
+          </div>
           {todos.map(todo => (
-            <p className="text-lg mt-3">{todo}</p>
+            <p className='text-lg mt-3'>{todo}</p>
           ))}
-
-
         </div>
       </div>
     </div>
-  )
+  );
 }
